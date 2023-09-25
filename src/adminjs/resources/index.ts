9408b4d7fbad7b8e0c66a1d79fbf2f7adb8058fc
Luis Filipe <luis_filipezm@hotmail.com>
2023-09-25 11:34:23 -0300

    Relacionamento models

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -1,10 +1,15 @@
 import { ResourceWithOptions } from 'adminjs';
-import { Category } from '../../models';
+import { Category, Course } from '../../models';
 import { categoryResurceOptions } from './category';
+import { courseResourceOptions } from './course';
 
 export const adminJsResources: ResourceWithOptions[] = [
     {
         resource: Category,
         options: categoryResurceOptions
+    },
+    {
+        resource: Course,
+        options: courseResourceOptions
     }
 ];
