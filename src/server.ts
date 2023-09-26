9abc462d2b9137db7a64553c866caaa39a145eb7
Luis Filipe <luis_filipezm@hotmail.com>
2023-09-26 13:59:10 -0300

    Registro de usuarios

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,26 @@
-import express from "express";
-import { adminJs, adminJsRouter } from "./adminjs";
-import { sequelize } from "./database";
-import { router } from "./routes";
+import express from "express"
+import { adminJs, adminJsRouter } from "./adminjs"
+import { sequelize } from "./database"
+import { router } from "./routes"
 
-const app = express();
+const app = express()
 
-app.use(express.static('public'));
+app.use(express.static('public'))
 
-app.use(adminJs.options.rootPath, adminJsRouter);
+app.use(express.json())
 
-app.use(router);
+app.use(adminJs.options.rootPath, adminJsRouter)
 
-const PORT = process.env.PORT || 3000;
+app.use(router)
+
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
     sequelize.authenticate().then(() => {
-        console.log('DB Connection has been established successfully.');
+        console.log('DB Connection has been established successfully.')
     })
 
-    console.log(`Server is running in http://localhost:${PORT}`);
+    console.log(`Server is running in http://localhost:${PORT}`)
 
-});
+})
 
