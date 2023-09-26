a2f7373890b09550d921e01699847ddf1475497d
Luis Filipe <luis_filipezm@hotmail.com>
2023-09-26 14:33:09 -0300

    Login com JWT auth

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { userService } from "../services/userService";
+import { jwtService } from "../services/jwtService";
+import { json } from "sequelize";
 
 export const authController = {
     register: async (req: Request, res: Response) => {
@@ -22,6 +24,34 @@ export const authController = {
                 role: 'user'
             })
             return res.status(201).json(user)
+        } catch (error) {
+            if (error instanceof Error){
+                return res.status(400).json({ message: error.message })
+            }
+        }
+    },
+
+    login: async (req: Request, res: Response) => {
+        const { email, password } = req.body
+
+        try {
+            const user = await userService.findByEmail(email)
+
+            if (!user) return res.status(404).json({ message: 'Usuário não encontrado.' })
+
+            user.checkPassword(password, (err, isSame) => {
+                if (err) return res.status(400).json({ message: err.message })
+                if (!isSame) return res.status(400).json({ message: 'Senha incorreta.' })
+            })
+
+            const payload = {
+                id: user.id,
+                firstNAme: user.firstName,
+                email: user.email,
+            }
+            const token = await jwtService.signToken(payload, '1d')
+
+            return res.json({ authenticated: true, ...payload, token })
         } catch (error) {
             if (error instanceof Error){
                 return res.status(400).json({ message: error.message })
