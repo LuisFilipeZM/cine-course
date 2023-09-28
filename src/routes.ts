44fb0b50612094e4dbd3a82c5b1a6f72e48ac2f0
Luis Filipe <luis_filipezm@hotmail.com>
2023-09-28 15:29:03 -0300

    Obtendo informações da conta de usuario

--- src/routes.ts
+++ src/routes.ts
@@ -34,6 +34,7 @@ router.delete('/favorites/:id', ensureAuth, favoritesController.delete)
 router.post('/likes', ensureAuth, likesController.save)
 router.delete('/likes/:id', ensureAuth, likesController.delete)
 
+router.get('/users/me', ensureAuth, usersController.show)
 router.get('/users/watching', ensureAuth, usersController.watching)
 
 export { router }
